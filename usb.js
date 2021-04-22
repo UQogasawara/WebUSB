b85f4c780a0c7f1d21128ec9ae647e02aecdd022
uQuest <78781704+uQuestSample@users.noreply.github.com>
2021-04-22 10:23:50 +0900

    Update usb.js

--- usb.js
+++ usb.js
@@ -1,7 +1,10 @@
   // グローバル変数
   let device;
-  let DevInfotable = document.getElementById('DevInfoTbl');
-  let cells = DevInfotable.querySelectorAll('td');
+  let DevInfoTable = document.getElementById('DevInfoTbl');
+  let DevInfoCells = DevInfoTable.querySelectorAll('td');
+
+  let DevConfigTable = document.getElementById('DevConfigTbl');
+  let DevConfigCells = DevConfigTable.querySelectorAll('td');
   
   // 指定デバイスのフィルタ
   const filters = [ 
@@ -18,6 +21,7 @@
   Connect_btn.addEventListener('click', async () => {
     try { 
       console.log('Connect_btn try.');
+      document.getElementById('ConsoleLog').innerText = "Connect_btn try.";
       device = await navigator.usb.requestDevice(
         {
           filters: filters
@@ -27,27 +31,31 @@
     catch (e) { 
       // No device was selected.
       console.log("Connect_btn There is no device. " + e);
+      document.getElementById('ConsoleLog').innerText = "Connect_btn There is no device. " + e;
     }
     if (device !== undefined) {
       // Add |device| to the UI. 
-      console.log('Add |device| to the UI.');
+      console.log('Connect_btn OK.');
+      document.getElementById('ConsoleLog').innerText = "Connect_btn OK.";
       }
     }
   );
   
-  // DevList_btn イベント
-  let DevList_btn = document.getElementById('DevList_btn');
-  DevList_btn.addEventListener('click', async () => {
+  // DevInfo_btn イベント
+  let DevInfo_btn = document.getElementById('DevInfo_btn');
+  DevInfo_btn.addEventListener('click', async () => {
     try { 
-      console.log('DevList_btn try.');
+      console.log('DevInfo_btn try.');
+      document.getElementById('ConsoleLog').innerText = "DevInfo_btn try.";
     }
     catch (e) { 
       // No device was selected.
-      console.log("DevList_btn There is no device. " + e);
+      console.log("DevInfo_btn There is no device. " + e);
+      document.getElementById('ConsoleLog').innerText = "DevInfo_btn There is no device. " + e;
     }
     if (device !== undefined) {
       // Add |device| to the UI. 
-
+      
       console.log("USB Ver "           + device.usbVersionMajor + "." + device.usbVersionMinor + device.usbVersionMinor);
       console.log("Class: 0x"          + device.deviceClass.toString(16));
       console.log("Subclass: 0x"       + device.deviceSubclass.toString(16));
@@ -58,22 +66,49 @@
       console.log("manufacturerName: " + device.manufacturerName);
       console.log("productName: "      + device.productName);
       console.log("serialNumber: "     + device.serialNumber);
-//      console.log("Product name: "   + device.configuration);
-//      console.log("Product name: "   + device.configurations);
       console.log("opened ? "          + device.opened);
 
+      DevInfoCells[1].innerText=device.usbVersionMajor + "." + device.usbVersionMinor + device.usbVersionMinor
+      DevInfoCells[3].innerText="0x"+device.deviceClass.toString(16)
+      DevInfoCells[5].innerText="0x"+device.deviceSubclass.toString(16)
+      DevInfoCells[7].innerText="0x"+device.deviceProtocol.toString(16)
+      DevInfoCells[9].innerText="0x"+device.vendorId.toString(16)
+      DevInfoCells[11].innerText="0x"+device.productId.toString(16)
+      DevInfoCells[13].innerText="0x"+device.deviceVersionMajor + "." + device.deviceVersionMinor + device.deviceVersionSubminor
+      DevInfoCells[15].innerText=device.manufacturerName
+      DevInfoCells[17].innerText=device.productName
+      DevInfoCells[19].innerText=device.serialNumber
 
-    cells[1].innerText=device.usbVersionMajor + "." + device.usbVersionMinor + device.usbVersionMinor
-    cells[3].innerText="0x"+device.deviceClass.toString(16)
-    cells[5].innerText="0x"+device.deviceSubclass.toString(16)
-    cells[7].innerText="0x"+device.deviceProtocol.toString(16)
-    cells[9].innerText="0x"+device.vendorId.toString(16)
-    cells[11].innerText="0x"+device.productId.toString(16)
-    cells[13].innerText="0x"+device.deviceVersionMajor + "." + device.deviceVersionMinor + device.deviceVersionSubminor
-    cells[15].innerText=device.manufacturerName
-    cells[17].innerText=device.productName
-    cells[19].innerText=device.serialNumber
+      DevConfigCells[1].innerText=device.configuration.configurationValue
 
       }
     }
   );
+
+function connectFunction() {
+    console.log('XXXXXXXXXXXXXXXXXXXXXXX.');
+}
+function disconnectFunction() {
+    console.log('YYYYYYYYYYYYYYYYYYYYYYY.');
+}
+
+navigator.usb.onconnect    = connectFunction
+navigator.usb.ondisconnect = disconnectFunction
+
+  // OpenDev_btn イベント
+  let OpenDev_btn = document.getElementById('OpenDev_btn');
+  OpenDev_btn.addEventListener('click', async () => {
+    try { 
+      console.log('OpenDev_btn try.');
+      document.getElementById('ConsoleLog').innerText = "OpenDev_btn try.";
+      await device.open()
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+
