98a2239e1cbdee3cc6e653019ba6b36e76d4b9df
uQuest <78781704+uQuestSample@users.noreply.github.com>
2021-05-13 15:04:06 +0900

    Update usb.js

--- usb.js
+++ usb.js
@@ -1,4 +1,4 @@
-  // グローバル変数
+ // グローバル変数
   let device;
   let DevInfoTable = document.getElementById('DevInfoTbl');
   let DevInfoCells = DevInfoTable.querySelectorAll('td');
@@ -265,3 +265,247 @@
       }
     }
   );
+
+  // CloseDev_btn イベント
+  let CloseDev_btn = document.getElementById('CloseDev_btn');
+  CloseDev_btn.addEventListener('click', async () => {
+    try { 
+      console.log('CloseDev_btn try.');
+      document.getElementById('ConsoleLog').innerText = "CloseDev_btn try.";
+      await device.close()
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+  
+  // SelectConfig_btn イベント
+  let SelectConfig_btn = document.getElementById('SelectConfig_btn');
+  SelectConfig_btn.addEventListener('click', async () => {
+    try { 
+      let SelectConfigNumber = document.getElementById('SelectConfigNumber').value;
+      console.log('SelectConfig_btn try.');
+      document.getElementById('ConsoleLog').innerText = "SelectConfig_btn try.";
+      await device.selectConfiguration(SelectConfigNumber)
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+  // ClaimInterface_btn イベント
+  let ClaimInterface_btn = document.getElementById('ClaimInterface_btn');
+  ClaimInterface_btn.addEventListener('click', async () => {
+    try { 
+      let ClaimInterfaceNumber = document.getElementById('ClaimInterfaceNumber').value;
+      console.log('ClaimInterface_btn try.');
+      document.getElementById('ConsoleLog').innerText = "ClaimInterface_btn try.";
+      await device.claimInterface(ClaimInterfaceNumber)
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+  // ReleaseInterface_btn イベント
+  let ReleaseInterface_btn = document.getElementById('ReleaseInterface_btn');
+  ReleaseInterface_btn.addEventListener('click', async () => {
+    try { 
+      let ReleaseInterfaceNumber = document.getElementById('ReleaseInterfaceNumber').value;
+      console.log('ReleaseInterface_btn try.');
+      document.getElementById('ConsoleLog').innerText = "ReleaseInterface_btn try.";
+      await device.releaseInterface(ReleaseInterfaceNumber)
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+  // DTR_RTS_btn イベント
+  let DTR_RTS_btn = document.getElementById('DTR_RTS_btn');
+  DTR_RTS_btn.addEventListener('click', async () => {
+    try { 
+      console.log('DTR_RTS_btn try.');
+      document.getElementById('ConsoleLog').innerText = "DTR_RTS_btn try.";
+      
+      await device.controlTransferOut({
+        requestType:'class',
+        recipient: 'interface',
+        request: 0x22,
+        value: 0x03,
+        index: 0x00
+      })
+      
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+  function toHex(v) {
+    return '0x' + (('0000' + v.toString(16).toUpperCase()).substr(-4));
+  }
+
+  // SerialSet_btn イベント
+  let SerialSet_btn = document.getElementById('SerialSet_btn');
+  SerialSet_btn.addEventListener('click', async () => {
+    try { 
+      console.log('SerialSet_btn try.');
+      document.getElementById('ConsoleLog').innerText = "SerialSet_btn try.";
+      
+      var dwDTERate;
+      var bCharFormat;
+      var bParityType;
+      var bDataBits;
+      
+      // ボーレート の設定
+      var element = document.getElementsByName('dwDTERate');
+      for ( var a="", i=element.length; i--; ) {
+        if ( element[i].checked ) {
+          dwDTERate = element[i].value ;
+          break;
+        }
+      }
+      // ストップビット の設定
+      var element = document.getElementsByName('bCharFormat');
+      for ( var a="", i=element.length; i--; ) {
+        if ( element[i].checked ) {
+          bCharFormat = element[i].value ;
+          break;
+        }
+      }
+      // パリティ の設定
+      var element = document.getElementsByName('bParityType');
+      for ( var a="", i=element.length; i--; ) {
+        if ( element[i].checked ) {
+          bParityType = element[i].value ;
+          break;
+        }
+      }
+      // データ の設定
+      var element = document.getElementsByName('bDataBits');
+      for ( var a="", i=element.length; i--; ) {
+        if ( element[i].checked ) {
+          bDataBits = element[i].value ;
+          break;
+        }
+      }
+      
+      var borate;
+      
+      // ボーレート16進数変換
+      switch(dwDTERate){
+        case "9600":
+          borate = toHex(9600);
+          break;
+        case "19200":
+          borate = toHex(19200);
+          break;
+        case "38400":
+          borate = toHex(38400);
+          break;
+        default :
+          borate = toHex(9600);
+          break;
+        }
+      
+      var borate_0 = ((borate & 0x000000FF) >>  0);
+      var borate_1 = ((borate & 0x0000FF00) >>  8);
+      var borate_2 = ((borate & 0x00FF0000) >> 16);
+      var borate_3 = ((borate & 0xFF000000) >> 24);
+      
+      var SerialSet_packet = Uint8Array.of(
+        borate_0, 
+        borate_1, 
+        borate_2, 
+        borate_3, 
+        bCharFormat, 
+        bParityType, 
+        bDataBits)
+
+      await device.controlTransferOut({
+        requestType:'class',
+        recipient: 'interface',
+        request: 0x20,
+        value: 0x00,
+        index: 0x00
+      }, SerialSet_packet)
+      
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+
+  function ascii (a) { return a.charCodeAt(0); }
+
+  // BulkOutTransfer_btn イベント
+  // 暫定的に 1Byte 固定にしている
+  let BulkOutTransfer_btn = document.getElementById('BulkOutTransfer_btn');
+  BulkOutTransfer_btn.addEventListener('click', async () => {
+    try { 
+      let BulkOutTransferEndpoint = document.getElementById('BulkOutTransferEndpoint').value;
+      let BulkOutTransferData = document.getElementById('BulkOutTransferData').value;
+
+      const BulkOutAry = Uint8Array.of(BulkOutTransferData)
+      
+      for ( var a="", i=BulkOutTransferData.length; i--; ) {
+        var asciiData = ascii(BulkOutTransferData);
+//        BulkOutAry.push(asciiData);
+      }
+      
+      console.log('BulkOutTransfer_btn try.');
+      document.getElementById('ConsoleLog').innerText = "BulkOutTransfer_btn try.";
+      await device.transferOut(BulkOutTransferEndpoint,BulkOutAry)
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
+  // BulkInTransfer_btn イベント
+  let BulkInTransfer_btn = document.getElementById('BulkInTransfer_btn');
+  BulkInTransfer_btn.addEventListener('click', async () => {
+    try { 
+      let BulkInTransferEndpoint = document.getElementById('BulkInTransferEndpoint').value;
+      let BulkInTransferLength = document.getElementById('BulkInTransferLength').value;
+      let BulkInTransferData = document.getElementById('BulkInTransferData').value;
+      
+      console.log('BulkInTransfer_btn try.');
+      document.getElementById('ConsoleLog').innerText = "BulkInTransfer_btn try.";
+      USBInTransferResult = await device.transferIn(BulkInTransferEndpoint,BulkInTransferLength)
+      
+      document.getElementById('BulkInTransferData').value = String.fromCharCode(USBInTransferResult.data.getUint8());
+//      var tmp = toHex(USBInTransferResult.data.getUint8());
+//      document.getElementById('BulkInTransferData').value = tmp;
+    }
+    catch (e) { 
+      // No device was selected.
+      console.log(" " + e);
+      document.getElementById('ConsoleLog').innerText = e;
+      }
+    }
+  );
+
